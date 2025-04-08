30666e7b906304ebb0c0e833fc365364bbdb7ddf
Kaddlington <kadd.grantblissett@gmail.com>
2025-04-08 04:57:42 +0100

    Bot Initialization Coded and Tested. Bot Comes online!

--- bot/bot.go
+++ bot/bot.go
@@ -3,27 +3,40 @@ package bot
 import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 	"log"
 	"os"
 	"os/signal"
 	"strings"
+	"syscall"
 )
 
 func Run() {
-	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error loading .env file")
+	}
+
+	discord, err := discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		log.Fatal("Failed to load BOT_TOKEN from environment variable", err)
 	}
 
 	discord.AddHandler(ping)
 
+	discord.Identify.Intents = discordgo.IntentsGuildMessages
+
 	discord.Open()
-	defer discord.Close()
+	if err != nil {
+		log.Fatal("Failed to open discord connection", err)
+	}
 
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	<-c
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	discord.Close()
 }
 
 func ping(s *discordgo.Session, m *discordgo.MessageCreate) {
